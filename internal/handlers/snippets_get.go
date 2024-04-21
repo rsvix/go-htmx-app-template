89708b3c4158fa84a33d25b32ddcbe6e68671e6a
ronaldo.domingues <ronaldo.domingues@autoglass.com.br>
2024-04-21 19:47:15 -0300

    .

--- internal/handlers/snippets_get.go
+++ internal/handlers/snippets_get.go
@@ -1,7 +1,10 @@
 package handlers
 
 import (
+	"log"
+
 	"github.com/labstack/echo/v4"
+	"github.com/rsvix/go-htmx-app-template/internal/structs"
 	"github.com/rsvix/go-htmx-app-template/internal/templates"
 )
 
@@ -16,6 +19,35 @@ func GetSnippetsHandler() *getSnippetsHandlerParams {
 }
 
 func (h getSnippetsHandlerParams) Serve(c echo.Context) error {
+
+	snippets := []structs.Snippet{
+		{
+			Id:       1,
+			Name:     "test snippet",
+			Category: "public",
+			Language: "golang",
+			Owner:    "me",
+			Content:  "asdasd",
+		},
+		{
+			Id:       2,
+			Name:     "test snippet 2",
+			Category: "public",
+			Language: "rust",
+			Owner:    "you",
+			Content:  "asdascascasd",
+		},
+		{
+			Id:       3,
+			Name:     "test snippet 3",
+			Category: "private",
+			Language: "python",
+			Owner:    "john",
+			Content:  "1231231232",
+		},
+	}
+	log.Println(snippets)
+
 	userName := c.Get("userName").(string)
-	return templates.SnippetsPage(c, h.pageTitle, userName).Render(c.Request().Context(), c.Response())
+	return templates.SnippetsPage(c, h.pageTitle, userName, snippets).Render(c.Request().Context(), c.Response())
 }
---
+++ internal/structs/snippet.go
@@ -0,0 +1,10 @@
+package structs
+
+type Snippet struct {
+	Id       uint
+	Name     string
+	Category string
+	Language string
+	Owner    string
+	Content  string
+}

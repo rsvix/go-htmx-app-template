595dab1532b644b408cc6ff2b95efeb9e7f565e6
ronaldo.domingues <ronaldo.domingues@autoglass.com.br>
2024-04-22 11:29:30 -0300

    .

--- internal/templates/snippet_templ.go
+++ internal/templates/snippet_templ.go
@@ -41,7 +41,7 @@ func SnippetModal(c echo.Context, snippetName string, snippetLang string, snippe
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
-		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("</h2><pre class=\"w-full text-sm font-normal leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white text-center my-4\">")
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("</h2><div class=\"w-full text-xs font-normal leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white text-center my-4\"><pre>")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
@@ -70,13 +70,13 @@ func SnippetModal(c echo.Context, snippetName string, snippetLang string, snippe
 		var templ_7745c5c3_Var5 string
 		templ_7745c5c3_Var5, templ_7745c5c3_Err = templ.JoinStringErrs(snippetContent)
 		if templ_7745c5c3_Err != nil {
-			return templ.Error{Err: templ_7745c5c3_Err, FileName: `internal/templates/snippet.templ`, Line: 28, Col: 36}
+			return templ.Error{Err: templ_7745c5c3_Err, FileName: `internal/templates/snippet.templ`, Line: 27, Col: 40}
 		}
 		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var5))
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
-		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("</code></pre><br><div class=\"flex flex-col items-center my-4\"><button _=\"on click trigger closeSnippetModal\" class=\"w-32 text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800\">Close</button></div></div></div><script type=\"text/javascript\" nonce=\"")
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("</code></pre></div><br><div class=\"flex flex-col items-center my-4\"><button _=\"on click trigger closeSnippetModal\" class=\"w-32 text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800\">Close</button></div></div></div><script type=\"text/javascript\" nonce=\"")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
@@ -89,7 +89,7 @@ func SnippetModal(c echo.Context, snippetName string, snippetLang string, snippe
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
-		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("\">\n        hljs.highlightAll()\n    </script>")
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("\">\n        hljs.highlightAll();\n    </script>")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}

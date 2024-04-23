7b4b74b184e4810bb1fb822272ae52be39304614
ronaldo.domingues <ronaldo.domingues@autoglass.com.br>
2024-04-23 19:46:59 -0300

    .

--- internal/handlers/snippetedit_delete.go
+++ internal/handlers/snippetedit_delete.go
@@ -1,8 +1,10 @@
 package handlers
 
 import (
+	"log"
 	"net/http"
 
+	"github.com/labstack/echo-contrib/session"
 	"github.com/labstack/echo/v4"
 	"gorm.io/gorm"
 )
@@ -20,13 +22,27 @@ func DeleteSnippetEditHandler() *deleteSnippetEditHandlerParams {
 func (h deleteSnippetEditHandlerParams) Serve(c echo.Context) error {
 	snippetId := c.Param("id")
 
-	db := c.Get("__db").(*gorm.DB)
-	var result struct {
-		Name     string
-		Language string
-		Code     string
+	session, err := session.Get("authenticate-sessions", c)
+	if err != nil {
+		log.Printf("Error getting session: %v\n", err)
+		return c.HTML(http.StatusInternalServerError, "<h2>Error, please try again</h2>")
 	}
-	db.Raw("DELETE FROM snippets WHERE id = ?;", snippetId).Scan(&result)
+
+	var userIdString string
+	if value, ok := session.Values["id"].(string); ok {
+		userIdString = value
+	} else {
+		return c.HTML(http.StatusInternalServerError, "<h2>Error, please try again</h2>")
+	}
+
+	db := c.Get("__db").(*gorm.DB)
+
+	var owner string
+	db.Raw("SELECT owner FROM snippets WHERE id = ?;", userIdString).Scan(&owner)
+
+	if owner == userIdString {
+		db.Raw("DELETE FROM snippets WHERE id = ?;", snippetId)
+	}
 
 	return c.Redirect(http.StatusSeeOther, "/snippets")
 }

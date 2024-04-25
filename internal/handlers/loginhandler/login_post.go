1cc090314423e4cc9096dfdb9a3b8d272fd310b1
ronaldo.domingues <ronaldo.domingues@autoglass.com.br>
2024-04-25 10:58:47 -0300

    .

--- internal/handlers/loginhandler/login_post.go
+++ internal/handlers/loginhandler/login_post.go
@@ -37,13 +37,12 @@ func (h *postLoginHandlerParams) Serve(c echo.Context) error {
 
 	db := c.Get(h.dbKey).(*gorm.DB)
 	var result struct {
-		Id        int
-		Firstname string
-		Password  string
-		Enabled   int
+		Id       int
+		Username string
+		Password string
+		Enabled  int
 	}
-	// db.Raw("SELECT password, enabled FROM users WHERE email = $1", email).Scan(&result)
-	db.Table("users").Select("id", "firstname", "password", "enabled").Where("email = ?", email).Scan(&result)
+	db.Raw("SELECT id, username, password, enabled FROM users WHERE email = ?", email).Scan(&result)
 	// log.Printf("result: %v\n", result)
 
 	if result.Id != 0 {
@@ -54,18 +53,17 @@ func (h *postLoginHandlerParams) Serve(c echo.Context) error {
 		// if hash.CheckPasswordHashV1(password, result.Password) {
 		if hash.CheckPasswordHashV2(password, result.Password) {
 
-			// Get session
 			session, err := session.Get("authenticate-sessions", c)
 			if err != nil {
 				log.Printf("Error getting session: %v\n", err)
 				return c.HTML(http.StatusInternalServerError, "<h2>Error, please try again</h2>")
 			}
 
-			// Set session values
 			session.Values["authenticated"] = true
-			session.Values["email"] = email
 			session.Values["id"] = strconv.FormatUint(uint64(result.Id), 10)
-			session.Values["firstname"] = result.Firstname
+			session.Values["email"] = email
+			session.Values["username"] = result.Username
+
 			if remember == "true" {
 				session.Options.MaxAge = 84600 * 30
 			}

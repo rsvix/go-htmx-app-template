9d6a4ba300b0da47ed759a3ae326dabe9c3e5fde
ronaldo.domingues <ronaldo.domingues@autoglass.com.br>
2024-04-27 11:03:00 -0300

    .

--- cmd/main.go
+++ cmd/main.go
@@ -124,7 +124,7 @@ func main() {
 	app.GET("/activate", activationhandler.GetActivateHandler().Serve, middlewares.MustNotBeLogged())
 	app.GET("/newactivation", activationhandler.GetNewActivationHandler().Serve, middlewares.MustNotBeLogged())
 	app.GET("/actvtkn", activationhandler.GetActivationTokenHandler().Serve, middlewares.MustNotBeLogged())
-	// app.GET("/activationtkn/:token", activationhandler.GetActivationTokenHandler().Serve, middlewares.MustNotBeLogged())
+	// app.GET("/actvtkn/:token", activationhandler.GetActivationTokenHandler().Serve, middlewares.MustNotBeLogged())
 
 	app.GET("/notfound", notfoundhandler.GetNotfoundHandler().Serve)
 	app.GET("/error", internalerrorhandler.GetInternalErrorHandler().Serve)

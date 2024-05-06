97bc85aed3974004533460cd7338d77e5a7e2e26
ronaldo.domingues <ronaldo.domingues@autoglass.com.br>
2024-05-06 18:37:43 -0300

    .

--- internal/templates/snippet_edit_templ.go
+++ internal/templates/snippet_edit_templ.go
@@ -62,7 +62,7 @@ func SnippetEditModal(snippetId string, snippetName string, snippetLang string, 
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
-		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("\" hx-target=\"body\"><textarea type=\"text\" name=\"snippetContent\" value=\"snippetContent\" class=\"p-4 w-full rounded-lg\" rows=\"15\">")
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("\" hx-target=\"body\"><textarea type=\"text\" name=\"snippetContent\" value=\"snippetContent\" class=\"p-4 w-full rounded-lg\" rows=\"8\">")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
